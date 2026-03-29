0a0050e8037bf6fddd9f5125e77762c2ef134a0e
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-03-29 15:52:29 +0545

    tailwind

--- my-app/app/layout.js
+++ my-app/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar.js"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +21,12 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`h-full`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+      <Navbar/>
+      {children}
+      </body>
     </html>
   );
 }
---
+++ my-app/components/navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar(){
+	return (
+		<nav className="[&_*]:list-none">
+			<ul><li>to do app</li></ul>
+		</nav>
+	)
+}
