f4f063778f921e87c560dcf79a95aab9b9ba3807
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-03-30 17:30:51 +0545

    theme

---
+++ todo/components/ThemeToggle.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function ThemeToggle() {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const saved = localStorage.getItem("theme");
+    if (saved === "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    }
+  }, []);
+
+  const toggleTheme = () => {
+    if (isDark) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    }
+    setIsDark(!isDark);
+  };
+
+  return (
+    <button className="dark:bg-[#010101] bg-[#EEEEEE]" onClick={toggleTheme}>
+      {isDark ? "🌙" : "☀️"}
+    </button>
+  );
+}
