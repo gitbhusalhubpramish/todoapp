d458d7f0c287e390d4b6225a276486b69b032481
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-03-31 16:29:02 +0545

    theme fixed

--- todo/tailwind.config.js
+++ todo/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",       // for Next.js app dir
     "./pages/**/*.{js,ts,jsx,tsx}",     // for pages dir
