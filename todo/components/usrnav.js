dac215d6368a8d435dbaf4b07d454e4ab9b9f16a
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-02 17:33:45 +0545

    link to login

--- todo/components/usrnav.js
+++ todo/components/usrnav.js
@@ -1,4 +1,5 @@
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 async function checkSession() {
     const cookieStore = await cookies(); // ✅ FIX
@@ -29,12 +30,12 @@ export default async function UsrNav(){
 	console.log("login: ",login)
 	return !login ? (
 		<div className="m-5 w-1/4 flex justify-evenly text-white">
-			<button className="bg-[#26a85a] dark:bg-gray-500 p-2 w-20 text-center dark:text-[#04060d] text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer dark:hover:bg-gray-600 hidden sm:inline-block hover:bg-[#228e4d]">
+			<Link className="bg-[#26a85a] dark:bg-gray-500 p-2 w-20 text-center dark:text-[#04060d] text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer dark:hover:bg-gray-600 hidden sm:inline-block hover:bg-[#228e4d]" href="/login">
 				Log In
-			</button>
-			<button className="dark:bg-green-500 p-2 w-20 text-center text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer bg-[#a6ffc9] dark:hover:bg-[#00A843] hover:bg-[#67ffa3]">
+			</Link>
+			<Link className="dark:bg-green-500 p-2 w-20 text-center text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer bg-[#a6ffc9] dark:hover:bg-[#00A843] hover:bg-[#67ffa3]" href="/signup">
 				Sign Up
-			</button>
+			</Link>
 		</div>
 	) : (
 		<div className="m-5 w-1/4 flex justify-evenly">
