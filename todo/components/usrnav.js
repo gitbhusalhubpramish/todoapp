07956a89ff666f65c229988ed708249c488a2a93
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-02 16:10:22 +0545

    button css

--- todo/components/usrnav.js
+++ todo/components/usrnav.js
@@ -12,11 +12,11 @@ export default async function UsrNav(){
 	console.log("login: ",login)
 	return (
 		<div className="m-5 w-1/4 flex justify-evenly text-white">
-			<button className="bg-[#26a85a] dark:bg-gray-500 p-2 w-20 text-center dark:text-[#04060d] text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer dark:hover:bg-gray-600 hidden sm:inline-block">
-				login
+			<button className="bg-[#26a85a] dark:bg-gray-500 p-2 w-20 text-center dark:text-[#04060d] text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer dark:hover:bg-gray-600 hidden sm:inline-block hover:bg-[#228e4d]">
+				Log In
 			</button>
-			<button className="dark:bg-green-500 p-2 w-20 text-center text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer bg-[#a6ffc9] ">
-				sign up
+			<button className="dark:bg-green-500 p-2 w-20 text-center text-[#004f1f] rounded-md border-1 dark:border-[#04060d] border-[#004f1f] cursor-pointer bg-[#a6ffc9] dark:hover:bg-[#00A843] hover:bg-[#67ffa3]">
+				Sign Up
 			</button>
 		</div>
 	)
