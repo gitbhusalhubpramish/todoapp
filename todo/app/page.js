e2f280289cb9d71a8e754ae1ee5308135543f3f8
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-12 13:38:11 +0545

    square tick

--- todo/app/page.js
+++ todo/app/page.js
@@ -1,21 +1,27 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <>
 	<div className="z-10 mt-30 w-full bg-[#dbffe9] dark:bg-[#0b1120] text-[#00bf00] flex flex-wrap overflow-x-hidden h-screen">
-		<div className="sm:max-ml-50 sm:max-mt-20 ml-10 mt-5 font-bold font-[montserrat]">
+		<div className="sm:ml-50 sm:mt-20 ml-10 mt-5 font-bold font-[montserrat] ">
 			<h1 className="sm:text-7xl text-6xl">Sttater</h1>
 			<h1 className="sm:text-9xl text-8xl">Walls,</h1>
 			<h1 className="sm:text-7xl mt-1 text-6xl">Stack</h1>
 			<h1 className="sm:text-9xl text-8xl">Bricks.</h1>
 		</div>
-		<div className="flex-1">
-			<div className="h-2/3 sm:flex justify-center items-center w-full ">
-				<div className="m-15 size-50 border-30 rounded-3xl border-[#00bf00] flex">
+		<div className="flex-1 ">
+			<div className="h-2/3 sm:flex justify-center items-center w-full flex-col">
+				<div className="m-15 aspect-square size-50 border-30 rounded-3xl border-[#00bf00] flex">
 					<div className="h-11/10 w-4/10 bg-[#00bf00] -rotate-30 rounded-full border-10 border-[#dbffe9] dark:border-[#0b1120]"/>
 					<div className="h-2/1 w-4/10 relative rotate-50 bottom-24 left-16 rounded-full flex items-end bg-[#dbffe9] dark:bg-[#0b1120]">
 						<div className="bg-[#00bf00] w-8/10 h-9/10 relative rounded-full"/>
 					</div>
 				</div>
+			{/*<div>
+				<p>
+					get started by <Link href="/login">logging in</Link> or <Link href="signin">signing up</Link>
+				</p>
+			</div>*/}
 			</div>
 		</div>
 	</div>
