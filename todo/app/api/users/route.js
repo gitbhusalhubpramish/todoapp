c3ee4cbe129e3056a6351f3ae07aae9bfbbf5ed0
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-21 14:36:10 +0545

    removed notificatioin, id and uid

--- todo/app/api/users/route.js
+++ todo/app/api/users/route.js
@@ -1,13 +1,22 @@
 import clientPromise from "@/lib/mongodb";
 
 export async function POST(req) {
-    const body = await req.json();   // ✅ await
+    const body = await req.json();
     const username = body.username;
 
     const client = await clientPromise;
     const db = client.db("projectdata");
 
-    const user = await db.collection("usrdata").findOne({ username }); // ✅ await
+    const user = await db.collection("usrdata").findOne(
+        { username },
+        {
+            projection: {
+                _id: 0,
+                notifications: 0,
+                userId: 0
+            }
+        }
+    );
 
     if (!user) {
         return Response.json({ error: "user not found" }, { status: 404 });
