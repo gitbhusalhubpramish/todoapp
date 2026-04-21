5a2ba48da1771a1c7251c51e35eb262fc90456f2
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-21 14:01:49 +0545

    bio

--- todo/app/api/migrate/route.js
+++ todo/app/api/migrate/route.js
@@ -4,50 +4,12 @@ export async function GET() {
     const client = await clientPromise;
     const db = client.db("projectdata");
 
-    const projects = db.collection("projects");
     const usrdata = db.collection("usrdata");
 
-    // 1. get all projects
-    const allProjects = await projects.find({}).toArray();
-
-    // 2. group by username
-    const map = new Map();
-
-    for (const p of allProjects) {
-        if (!p.owner) {
-            console.log("no user");
-            continue;
-        }
-
-        const key = p.owner; // ✅ FIXED
-
-        if (!map.has(key)) {
-            map.set(key, []);
-        }
-
-        map.get(key).push({
-            projectId: p._id,
-            title: p.content.title
-        });
-    }
-
-    // 3. update usrdata
-    const allUsers = await usrdata.find({}).toArray();
-
-    for (const user of allUsers) {
-        await usrdata.updateOne(
-            { username: user.username },
-            {
-                $set: {
-                    projects: map.get(user.username) || []
-                }
-            }
-        );
-    }
+    const users = await usrdata.find({}).toArray();
 
     return Response.json({
         success: true,
-        totalProjects: allProjects.length,
-        groupedUsers: map.size
+        data: users
     });
 }
