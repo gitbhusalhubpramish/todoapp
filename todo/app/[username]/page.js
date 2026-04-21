da3e2726886812b84dcde5769062b0e1d8de637a
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-04-21 16:44:57 +0545

    follow button

--- todo/app/[username]/page.js
+++ todo/app/[username]/page.js
@@ -45,9 +45,9 @@ export default function ProfilePage({ params }) {
 	
 
 	return (
-		<div className="min-h-screen bg-[#dbffe9] dark:bg-[#0b1120] text-black dark:text-white p-6">
-			<div className="flex justify-center">
-				<div>
+		<div className="min-h-screen bg-[#dbffe9] dark:bg-[#0b1120] text-black dark:text-white p-6 ">
+			<div className="flex justify-center flex-wrap">
+				<div className="sm:w-40 sm:h-40">
 					<Image
 						src={user?.profilepic || "/profile.svg"}
 						alt="profile"
@@ -57,11 +57,16 @@ export default function ProfilePage({ params }) {
 					/>
                 </div>
 				<div>
-					<h1 className="text-4xl h-1/2 flex items-end m-3">{user ? user.username : (<Skeleton className="w-30 h-5"/>)}</h1>
+					<h1 className="sm:text-4xl text-3xl h-1/2 flex items-end m-3">{user ? user.username : (<Skeleton className="w-30 h-5"/>)}</h1>
 					<div className="flex gap-2 m-3">
 						<div>{user ? (user.follower?.length ?? 0)+" followers" : <Skeleton className="h-4 w-10" />}</div>
 						<div>{user ? (user.following?.length ?? 0)+" following" : <Skeleton className="h-4 w-10" />} </div>
 					</div>
+				</div>
+				<div className="flex justify-center sm:justify-start items-center mt-4 sm:mt-0 sm:ml-6">
+					<button className="px-5 py-2 rounded-full font-medium text-sm transition-all duration-200 bg-green-600 text-white hover:bg-green-700 active:scale-95 shadow-md hover:shadow-lg dark:bg-green-500 dark:hover:bg-green-600">
+						Follow
+					</button>
 				</div>
 			</div>
 		</div>
