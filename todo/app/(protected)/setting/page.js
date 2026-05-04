29ce9fb5f0d297d92b55589e2cb691f0af5baf7b
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-04 20:08:13 +0545

    file init

---
+++ todo/app/(protected)/setting/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function setting(){
+	return (
+		<></>
+	)
+}
