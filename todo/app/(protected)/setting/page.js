ee559073347a1571c1568b2094a603094cb79a06
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-04 20:13:39 +0545

    idea

--- todo/app/(protected)/setting/page.js
+++ todo/app/(protected)/setting/page.js
@@ -3,7 +3,15 @@
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-
+//           pp      usrname
+//           bioooooooooo
+//  -----------------------------------
+//     project one 				del
+//     project 2 				del
+//====================================
+//  change pass
+//	del acc
+//	logout
 export default function setting(){
 	return (
 		<></>
