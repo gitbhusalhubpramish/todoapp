d385e3fceb518828502d173f6f97e7f30518855a
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-05 20:27:31 +0545

    hooks init

--- todo/app/(protected)/setting/page.js
+++ todo/app/(protected)/setting/page.js
@@ -22,6 +22,43 @@ export default function setting({params}){
 	const [loading, setLoading] = useState(true);
 	const [notFoundState, setNotFoundState] = useState(false);
 	const [session, setSessionUser] = useState(null);
+	
+	useEffect(() => {
+		async function loadSession() {
+			const res = await fetch("/api/me/auth");
+			const data = await res.json();
+			console.log("session raw data ",data)
+			setSessionUser(data.user);
+		}
+
+		loadSession();
+	}, []);
+	useEffect(() => {
+		async function loadUser() {
+			setLoading(true);
+
+			const res = await fetch(`/api/users/${username}/setting`);
+
+			if (res.status === 404) {
+				setNotFoundState(true);
+				setLoading(false);
+				return;
+			}
+
+			const data = await res.json();
+			console.log(data)
+			setUser(data.user);
+			setLoading(false);
+		}
+
+		loadUser();
+	}, [username, session]);
+	if (notFoundState){
+		notFound()
+	}
+	if (!user){
+		redirect("/login")
+	}
 	return (
 		<></>
 	)
