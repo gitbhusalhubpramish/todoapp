2c966ac88be3dcefe8839b8f9e66f0b1f299401d
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-15 14:04:06 +0545

    compression

--- todo/app/api/users/[username]/setting/pp/route.js
+++ todo/app/api/users/[username]/setting/pp/route.js
@@ -2,6 +2,7 @@ import clientPromise from "@/lib/mongodb";
 import { getCurrentUser } from "@/lib/auth";
 import { v2 as cloudinary } from "cloudinary";
 
+
 cloudinary.config(process.env.CLOUDINARY_URL);
 
 export async function POST(req, {params}){
@@ -16,26 +17,44 @@ export async function POST(req, {params}){
 		return Response.json({ error: "Unauthorized" }, { status: 401 });
 	}
 
-	const client = await clientPromise;
-	const db = client.db("projectdata");
+	
+	if (!file.type.startsWith("image/")) {
+		return Response.json({ error: "Invalid file" }, { status: 400 });
+	}
+	
 	
 	const uploaded = await new Promise((resolve, reject) => {
-		cloudinary.uploader
-			.upload_stream(
-				{
-					folder: `users/${username}`,
-					public_id: "image",
-					overwrite: true,
-				},
-				(error, result) => {
-					if (error) reject(error);
-					else resolve(result);
-				}
-			)
-			.end(buffer);
+	cloudinary.uploader
+		.upload_stream(
+			{
+				folder: `users/${username}`,
+				public_id: "image",
+				overwrite: true,
+
+				transformation: [
+					{
+						width: 512,
+						height: 512,
+						crop: "fill",
+						gravity: "face",
+
+						quality: "auto",
+						fetch_format: "auto",
+					},
+				],
+			},
+			(error, result) => {
+				if (error) reject(error);
+				else resolve(result);
+			}
+		)
+		.end(buffer);
 	});
 	
 	console.log(uploaded)
+	
+	const client = await clientPromise;
+	const db = client.db("projectdata");
 	
 	await db.collection("usrdata").updateOne(
 		{ username },
