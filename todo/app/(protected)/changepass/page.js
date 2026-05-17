951d282736030c74e3ca1133ff2f2a5ee1f1132f
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-17 16:23:18 +0545

    session

--- todo/app/(protected)/changepass/page.js
+++ todo/app/(protected)/changepass/page.js
@@ -1,4 +1,22 @@
+"use client";
+
+import { useEffect, useState, use } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
 export default function changepass(){
+	const [session, setSessionUser] = useState(null);
+	useEffect(() => {
+		async function loadSession() {
+			const res = await fetch("/api/me/auth");
+			const data = await res.json();
+			console.log("session raw data ",data)
+			setSessionUser(data.user);
+		}
+
+		loadSession();
+	}, []);
 	return (
 	
 	)
