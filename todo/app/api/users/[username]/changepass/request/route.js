6fb193903a60828c7370f3361fbabcca1e6f75be
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-18 17:47:52 +0545

    config: added rules for pass in be

--- todo/app/api/users/[username]/changepass/request/route.js
+++ todo/app/api/users/[username]/changepass/request/route.js
@@ -18,6 +18,30 @@ export async function POST(req, {params}){
 	
 	const { oldPassword, newPassword, confirmPassword } = body
 	
+	if (
+		!oldPassword.trim() ||
+		!newPassword.trim() ||
+		!confirmPassword.trim()
+	) {
+		return Response.json({error:"All fields are required"},{status: 401})
+	}
 	
-	
+		if (newPassword.length < 8) {
+		return Response.json({error:"Password must be at least 8 characters"},{status:401}
+	}
+	if (newPassword !== confirmPassword) {
+		return Response.json({error:"Passwords do not match"},{status:401})
+	}
+
+	if (oldPassword === newPassword) {
+		return Response.json({error:"New password must be different"},{status:401})
+	}
+
+	if (
+		!/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/.test(
+			newPassword
+		)
+	) {
+		return Response.json({error:"Weak password"},{status:401})
+	}
 }
