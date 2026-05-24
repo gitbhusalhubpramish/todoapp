a7f2bfa896219f694dfbd21e3b18c0dc72e76632
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-05-24 14:17:31 +0545

    fetch

--- todo/app/search/page.js
+++ todo/app/search/page.js
@@ -1,10 +1,42 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q") || "";
+	const searchParams = useSearchParams();
+	const query = searchParams.get("q") || "";
+	
+	const router = useRouter();
+	
+	const [results, setResults] = useState([]);
+  
+	if (query === ""){
+		router.push("/")
+	}
+	
+	useEffect(() => {
+		const timeout = setTimeout(async () => {
+			if (!query.trim()) {
+				setResults([]);
+				setOpen(false);
+				return;
+			}
 
-  return <div>Query: {query}</div>;
+			const res = await fetch(
+				`/api/search?q=${encodeURIComponent(query)}`
+			);
+
+			const data = await res.json();
+
+			setResults(data || []);
+		}, 300);
+
+		return () => clearTimeout(timeout);
+	}, [query]);
+	
+	console.log(results)
+	
+	return <div>Query: {query}</div>;
 }
