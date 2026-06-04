c8f00350f29a32781b99b50bd492af7c6cc1f49c
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-04 18:13:51 +0545

    comment

--- todo/app/api/logout/route.js
+++ todo/app/api/logout/route.js
@@ -3,18 +3,23 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
 export async function POST() {
+	//get cookie/sessionID
 	const cookieStore = await cookies();
 	const sessionId = await cookieStore.get("sessionId")?.value;
 
 	if (sessionId) {
+		//connect to database
 		const client = await clientPromise;
 		const db = client.db("projectdata"); 
 
+		//connect to collection
 		const sessions = db.collection("sessions");
 
+		//delete sessionID from collection
 		await sessions.deleteOne({ sessionId });
 	}
 
+	//delete sessionid form client cookies storage
 	cookieStore.set("sessionId", "", {
 		httpOnly: true,
 		expires: new Date(0),
