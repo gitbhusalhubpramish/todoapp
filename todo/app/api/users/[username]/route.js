267e8db7bf696f0d7011ee027ce4d1ae50f27e62
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-06 15:15:05 +0545

    comment

--- todo/app/api/users/[username]/route.js
+++ todo/app/api/users/[username]/route.js
@@ -1,11 +1,14 @@
 import clientPromise from "@/lib/mongodb";
 
 export async function GET(req, { params }) {
+	//get username
 	const { username } = await params;
 
+	// connect to database
 	const client = await clientPromise;
 	const db = client.db("projectdata");
 
+	//get userdata
 	const user = await db.collection("usrdata").findOne(
 		{ username },
 		{
@@ -16,48 +19,54 @@ export async function GET(req, { params }) {
 			}
 		}
 	);
-	const projects = await db.collection("projects").find({ owner: username }).toArray();
-
-	const formattedProjects = projects.map(p => ({
-		title: p.content?.title,
-		description: p.content?.description,
-		isdone: p.content?.isdone
-	}));
-
+	
+	// throw error on user doesn't exist in usrdata collection
 	if (!user) {
 		return Response.json(
 			{ error: "user not found" },
 			{ status: 404 }
 		);
 	}
+	
+	//find projects
+	const projects = await db.collection("projects").find({ owner: username }).toArray();
+
+	//format project form {title} to {title, description, isdone}
+	const formattedProjects = projects.map(p => ({
+		title: p.content?.title,
+		description: p.content?.description,
+		isdone: p.content?.isdone
+	}));
+	
+	//set project as formatted project
 	user.projects = formattedProjects
 	
+	//get liked project link
 	const liked = user.likedprojects.map((item) => {
 		const [owner, project] = item.split("/");
 		return { owner, project };
 	});
 	
-	console.log(liked)
-	
+	// get liked project detail form projects collection
 	const likedpro = await db.collection("projects").find({
 		"content.title": { $in: liked.map(p => p.project) },
 		owner: { $in: liked.map(p => p.owner) }
 	}).toArray()
 	
-	console.log(likedpro)
 	
+	//find for owner of project
 	const users = await db.collection("usrdata").find({
 		username: { $in: likedpro.map(p => p.owner) }
 	}).toArray()
 
-	console.log(users)
-	
+	//format usermpa
 	const userMap = Object.fromEntries(
 		users.map(u => [u.username, u.profilepic])
 	);
 	
 	console.log(userMap)
 	
+	//add profile pic too
 	const result = likedpro.map(p => ({
 		owner: p.owner,
 		profilepic: userMap[p.owner],
@@ -69,6 +78,7 @@ export async function GET(req, { params }) {
 	
 	console.log(result)
 	
+	//set changes
 	user.likedprojects = result
 
 	return Response.json({ user });
