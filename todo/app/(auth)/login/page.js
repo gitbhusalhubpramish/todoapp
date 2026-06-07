3e212e0adb7c34cfd2f00b7d922a756be86ae3fd
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-07 15:46:02 +0545

    indident

--- todo/app/(auth)/login/page.js
+++ todo/app/(auth)/login/page.js
@@ -11,11 +11,6 @@ export default function Signup() {
 	const [conformpass, setConformpass] = useState("")
 	const inputsRef = useRef([]);
 	
-	/*useEffect(() => {
-	setMounted(true);
-}, []);*/
-
-
 	const [form, setForm] = useState({
 		action:"",
 		username: "",
@@ -42,124 +37,122 @@ export default function Signup() {
 		}));
 	};
 	const handleChangeOTP = (value, index) => {
-	if (!/^\d?$/.test(value)) return;
+		if (!/^\d?$/.test(value)) return;
 
-	const newCode = [...form.code];
-	newCode[index] = value;
+		const newCode = [...form.code];
+		newCode[index] = value;
 
-	setForm((prev) => ({
-		...prev,
-		code: newCode,
-	}));
+		setForm((prev) => ({
+			...prev,
+			code: newCode,
+		}));
 
-	if (value && index < 5) {
-		inputsRef.current[index + 1]?.focus();
-	}
-};
-
-const handleKeyDownOTP = (e, index) => {
-	if (e.key === "Backspace" && !form.code[index] && index > 0) {
-		inputsRef.current[index - 1]?.focus();
-	}
-};
-
-const handlePasteOTP = (e) => {
-	e.preventDefault();
-
-	const pastedData = e.clipboardData
-		.getData("text")
-		.replace(/\D/g, "")
-		.slice(0, 6);
-
-	if (!pastedData) return;
-
-	const newCode = [...form.code];
-
-	pastedData.split("").forEach((digit, index) => {
-		newCode[index] = digit;
-	});
-
-	setForm((prev) => ({
-		...prev,
-		code: newCode,
-	}));
-
-	const lastIndex = Math.min(
-		pastedData.length - 1,
-		5
-	);
-
-	inputsRef.current[lastIndex]?.focus();
-};
-
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    setError("")
-    if (!e.target.checkValidity()) {
-		e.target.reportValidity();
-		return;
-	}
-	if (form.password !== conformpass && form.action === "forget"){
-		setError("Password Doesn't Match")
-		return
-	}
-    // 👉 trigger invisible captcha
-    if (!recaptchaRef.current) return;
-	recaptchaRef.current.execute();
-  };
-const handleCaptchaVerify = async (token) => {
-    try {
-      setCaptchaToken(token);
-
-      const res = await fetch("/api/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          ...form,
-          captchaToken: token,
-        }),
-      });
-		
-      const data = await res.json();
-      console.log(data);
-      
-      recaptchaRef.current?.reset();
-      setCaptchaToken(null);
-		if (res.status === 201){
-			alert("login success");
-			window.location.reload()
+		if (value && index < 5) {
+			inputsRef.current[index + 1]?.focus();
 		}
-      else if (res.status>=400){
-			setError(data.error)
+	};
+
+	const handleKeyDownOTP = (e, index) => {
+		if (e.key === "Backspace" && !form.code[index] && index > 0) {
+			inputsRef.current[index - 1]?.focus();
+		}
+	};
+
+	const handlePasteOTP = (e) => {
+		e.preventDefault();
+
+		const pastedData = e.clipboardData
+			.getData("text")
+			.replace(/\D/g, "")
+			.slice(0, 6);
+
+		if (!pastedData) return;
+
+		const newCode = [...form.code];
+
+		pastedData.split("").forEach((digit, index) => {
+			newCode[index] = digit;
+		});
+
+		setForm((prev) => ({
+			...prev,
+			code: newCode,
+		}));
+
+		const lastIndex = Math.min(
+			pastedData.length - 1,
+			5
+		);
+
+		inputsRef.current[lastIndex]?.focus();
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		setError("")
+		if (!e.target.checkValidity()) {
+			e.target.reportValidity();
+			return;
+		}
+		if (form.password !== conformpass && form.action === "forget"){
+			setError("Password Doesn't Match")
 			return
-			
-			//alert("Error: " + data.error);
 		}
-		
+		// 👉 trigger invisible captcha
+		if (!recaptchaRef.current) return;
+		recaptchaRef.current.execute();
+	};
+	const handleCaptchaVerify = async (token) => {
+		try {
+			setCaptchaToken(token);
+
+			const res = await fetch("/api/login", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({
+					...form,
+					captchaToken: token,
+				}),
+			});
+			
+			const data = await res.json();
+			console.log(data);
+      
+			recaptchaRef.current?.reset();
+			setCaptchaToken(null);
+			if (res.status === 201){
+				alert("login success");
+				window.location.reload()
+			}
+			else if (res.status>=400){
+				setError(data.error)
+				return
+			}
+			
 
       //if (!recaptchaRef.current) return;
-      if (form.action === "forget"){
-			setForm((prev)=>({
-				...prev,
-				action:"verify",
+			if (form.action === "forget"){
+				setForm((prev)=>({
+					...prev,
+					action:"verify",
+					password: "",
+					code:Array(6).fill(""),
+				}));
+				console.log(form)
+				return
+			}
+			setForm({
+				action:"",
+				username: "",
 				password: "",
 				code:Array(6).fill(""),
-			}));
-			console.log(form)
-			return
+			})
+		} catch (err) {
+			console.error(err);
+			recaptchaRef.current?.reset();
+			setCaptchaToken(null);
 		}
-      setForm({
-			action:"",
-			username: "",
-			password: "",
-			code:Array(6).fill(""),
-		})
-    } catch (err) {
-      console.error(err);
-      recaptchaRef.current?.reset();
-      setCaptchaToken(null);
-    }
-  };
+	};
 	
 
 	return (
@@ -172,69 +165,72 @@ const handleCaptchaVerify = async (token) => {
 					{form.action !=="" ? "reset password" : "Log In"}
 				</h1>
 				{error && (
-    <p className="text-red-500 text-sm font-medium text-center">
-      {error}
-    </p>
-  )}
+					<p className="text-red-500 text-sm font-medium text-center">
+						{error}
+					</p>
+				)}
 
 				{(["", "forget"].includes(form.action)) ? (
 					<>
-					{/* Username */}
-					<input
-						pattern="[a-zA-Z0-9_@.\-]+"
-						type="text"
-						name="username"
-						placeholder="Email or Username"
-						value={form.username}
-						onChange={handleChange}
-						required
-						className="p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
-					/>
-
-					{/* Password */}
-					<input
-						type="password"
-						name="password"
-						placeholder="Password"
-						value={form.password}
-						onChange={handleChange}
-						required
-						className="p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
-					/>
-					{form.action === "forget" && (
+						{/* Username */}
 						<input
-							type="password"
-							name="password"
-							placeholder="Conform Password"
-							value={conformpass}
-							onChange={(e)=>setConformpass(e.target.value)}
+							pattern="[a-zA-Z0-9_@.\-]+"
+							type="text"
+							name="username"
+							placeholder="Email or Username"
+							value={form.username}
+							onChange={handleChange}
 							required
 							className="p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
 						/>
-					)}
-					{form.action ==="" && (<p className="text-end text-blue-500"><button className="underline cursor-pointer" type="button" onClick={handelForget}>Forgot password?</button></p>)}</>
-				):(<>
-					<div className="flex gap-2">
-			{form.code.map((digit, i) => (
-				<input
-					key={i}
-					ref={(el) => (inputsRef.current[i] = el)}
-					type="text"
-					inputMode="numeric"
-					maxLength={1}
-					value={digit}
-					onChange={(e) => handleChangeOTP(e.target.value, i)}
-					onKeyDown={(e) => handleKeyDownOTP(e, i)}
-					onPaste={handlePasteOTP}
-					className="w-10 h-12 text-center text-lg p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
-				/>
-			))}
-		</div>
-				</>)}
+
+						{/* Password */}
+						<input
+							type="password"
+							name="password"
+							placeholder="Password"
+							value={form.password}
+							onChange={handleChange}
+							required
+							className="p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
+						/>
+						{form.action === "forget" && (
+							<input
+								type="password"
+								name="password"
+								placeholder="Conform Password"
+								value={conformpass}
+								onChange={(e)=>setConformpass(e.target.value)}
+								required
+								className="p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
+							/>
+						)}
+						{form.action ==="" && (<p className="text-end text-blue-500"><button className="underline cursor-pointer" type="button" onClick={handelForget}>Forgot password?</button></p>)}
+					</>
+				):(
+					<>
+						<div className="flex gap-2">
+							{form.code.map((digit, i) => (
+								<input
+									key={i}
+									ref={(el) => (inputsRef.current[i] = el)}
+									type="text"
+									inputMode="numeric"
+									maxLength={1}
+									value={digit}
+									onChange={(e) => handleChangeOTP(e.target.value, i)}
+									onKeyDown={(e) => handleKeyDownOTP(e, i)}
+									onPaste={handlePasteOTP}
+									className="w-10 h-12 text-center text-lg p-3 rounded-lg border border-gray-300 dark:border-gray-600 bg-transparent text-gray-600 dark:text-gray-300 placeholder:text-[#6b7280] focus:outline-none focus:ring-2 focus:ring-[#00bf00]"
+								/>
+							))}
+						</div>
+					</>
+				)}
 
 				{/* CAPTCHA */}
 				<ReCAPTCHA
-				ref={recaptchaRef}
+					ref={recaptchaRef}
 					sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
 					onChange={handleCaptchaVerify}
 					size="invisible"
