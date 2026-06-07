9a00809f98a826f167f796262cadcf4e8b53cb65
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-07 14:24:51 +0545

    removed console

--- todo/app/api/users/[username]/[project]/route.js
+++ todo/app/api/users/[username]/[project]/route.js
@@ -57,9 +57,8 @@ export async function PATCH(req, { params }) {
 
 	//connect to database collection
 	const client = await clientPromise;
-	const db = client.db("projectdata");
-	console.log(username, project)
-
+	const db = client.db("projectdata");]
+	
 	//get project form database collection
 	const doc = await db.collection("projects").findOne({
 		owner: username,
