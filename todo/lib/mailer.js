67c081c6852e1379e9e049a10fbf0ac339118d42
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-08 14:21:02 +0545

    refactor mailer

--- todo/lib/mailer.js
+++ todo/lib/mailer.js
@@ -1,15 +1,17 @@
 import { Resend } from "resend";
 
+//connect resend
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendResetEmail(to, code) {
+export async function sendResetEmail(to, code, text, subject) {
+	//send email
 	await resend.emails.send({
 		from: process.env.EMAIL_FROM,
 		to,
-		subject: "Password Reset Code",
+		subject: subject,
 		html: `
 			<div style="font-family: sans-serif">
-				<h2>Password Reset</h2>
+				<h2>${text}</h2>
 				<p>Your verification code is:</p>
 				<h1 style="letter-spacing: 4px">${code}</h1>
 				<p>This code expires in 10 minutes.</p>
