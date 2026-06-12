d712b4987cf9301ab5b39cf9239d02762a87e1b2
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-12 15:07:31 +0545

    return if unread notification

--- todo/app/api/users/[username]/newnot/route.js
+++ todo/app/api/users/[username]/newnot/route.js
@@ -2,10 +2,13 @@ import clientPromise from "@/lib/mongodb";
 import { getCurrentUser } from "@/lib/auth.js";
 
 export async function GET(){
-	const { username, project } = await params;
+	//get params
+	const { username } = await params;
 
+	//get cookies
 	const session = await getCurrentUser();
 
+	//permitinon validation
 	if (!session) {
 		return Response.json({ error: "Unauthorized" }, { status: 401 });
 	}
@@ -14,5 +17,15 @@ export async function GET(){
 		return Response.json({ error: "Forbidden" }, { status: 403 });
 	}
 	
+	//connect to database
+	const client = await clientPromise;
+	const db = client.db("projectdata");
 	
+	//search user in database collection
+	const user = await db.collection("usrdata").findOne(
+		{ username },
+		{ projection: { notifications: 1} }
+	);
+	
+	return Response.json(user.notifications[-1].isRead)
 }
