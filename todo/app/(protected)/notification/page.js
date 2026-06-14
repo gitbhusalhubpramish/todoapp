f1b2aa6ce286bd61ebec713757c56075e209b772
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 14:58:06 +0545

    title and des for notification

--- todo/app/(protected)/notification/page.js
+++ todo/app/(protected)/notification/page.js
@@ -1,5 +1,10 @@
 import Notifications from "./notification.js"
 
+export const metadata = {
+	title: "Notifications | Tick It",
+
+	description: "View project updates, task activity, follower interactions, and community notifications."
+};
 
 export default function NotificationsPage() {
 	return (
