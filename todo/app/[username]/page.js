283b8b1bf14f4c4603247d41a386c4553eb063a3
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 16:14:53 +0545

    profile page ttl and des

--- todo/app/[username]/page.js
+++ todo/app/[username]/page.js
@@ -1,5 +1,14 @@
 import Profile from "./profile.js"
 
+export async function generateMetadata({ params }) {
+	const { username } = await params;
+
+	return {
+		title: `${username} | Tick It`,
+		description: `View ${username}'s projects and profile on Tick It.`,
+	};
+}
+
 export default async function ProfilePage({ params }) {
 	const {username} = await params
 	return (
