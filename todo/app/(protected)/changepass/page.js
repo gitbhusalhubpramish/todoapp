7be2e5e294932793263992628ed15244e5f82d76
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 15:16:48 +0545

    zero index added

--- todo/app/(protected)/changepass/page.js
+++ todo/app/(protected)/changepass/page.js
@@ -1,6 +1,10 @@
 import ChangePassword from "./changepass.js"
 
 export const metadata = {
+	robots: {
+		index: false,
+		follow: false,
+	},
 	title: "Change Password | Tick It",
 
 	description: "Update your Tick It account password and keep your account secure."
