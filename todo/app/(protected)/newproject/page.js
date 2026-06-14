1fa6b09cbd28fd7d55b764aab78a20ff1a1d0356
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 14:23:45 +0545

    title and des for new project

--- todo/app/(protected)/newproject/page.js
+++ todo/app/(protected)/newproject/page.js
@@ -11,6 +11,12 @@ function sanitizeInput(value) {
 		.trim();
 }
 
+export const metadata = {
+	title: "Create a New Project | Tick It"
+
+	description:"Start a new project, organize tasks, document progress, and share your work with the Tick It community."
+};
+
 export default function NewProjectPage() {
 	//initlize router
 	const router = useRouter();
