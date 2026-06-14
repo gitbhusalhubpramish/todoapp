1e86365ec9d52a0a63000aeca077f326e77365be
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 15:30:29 +0545

    setting ttl and des

--- todo/app/(protected)/setting/page.js
+++ todo/app/(protected)/setting/page.js
@@ -1,4 +1,8 @@
 import Setting from "./setting.js"
+export const metadata = {
+	title: "Account Settings | Tick It",
+	description: "Manage your Tick It account settings, profile information, privacy preferences, security options, and account customization.",
+};
 export default function Settingpage(){
 	return (
 		<Setting/>
