c684206fca57385795e2f2d895a49f683b9d8635
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 14:21:31 +0545

    title and des for signup page

--- todo/app/(auth)/signup/page.js
+++ todo/app/(auth)/signup/page.js
@@ -3,6 +3,12 @@ import { useState, useRef } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import Link from "next/link";
 
+export const metadata = {
+	title: "Create Your Tick It Account"
+
+	description:"Join Tick It to organize tasks, share projects, build your portfolio, and connect with a productivity-focused community."
+};
+
 export default function Signup() {
 	
 	//ref
