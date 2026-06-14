a50bba94d645e8488759795d0a8d7ca41404f180
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 14:49:46 +0545

    created noindex

--- todo/app/(protected)/logout/page.js
+++ todo/app/(protected)/logout/page.js
@@ -1,6 +1,10 @@
 import LogoutButton from "./logout.js"
 
 export const metadata = {
+	robots: {
+		index: false,
+		follow: false,
+	},
 	title: "Sign Out | Tick It",
 
 	description:"Securely sign out of your Tick It account."
