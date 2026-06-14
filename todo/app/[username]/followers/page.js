db2dc12dc01929015f846df343321c8f3db2c419
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 17:05:00 +0545

    followers ttl and des

--- todo/app/[username]/followers/page.js
+++ todo/app/[username]/followers/page.js
@@ -1,4 +1,15 @@
 import Followers from "./followers.js"
+
+export async function generateMetadata({ params }) {
+	const { username } = await params;
+
+	return {
+		title: `${username}'s Followers | Tick It`,
+
+		description: `Explore people following ${username} and discover active creators and project builders.`
+	};
+}
+
 export default async function followerspage({ params }){
 	//get target username
 	const {username} = await params
