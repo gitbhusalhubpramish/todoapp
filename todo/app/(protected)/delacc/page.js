d45e5dee8623b9307dbc93c1e5bbe61d03ac2ade
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 15:20:31 +0545

    dle acc ttl and des

--- todo/app/(protected)/delacc/page.js
+++ todo/app/(protected)/delacc/page.js
@@ -1,4 +1,15 @@
 import DeleteAccount from "./delacc.js"
+
+export const metadata = {
+	robots: {
+		index: false,
+		follow: false,
+	},
+	title: "Delete Account | Tick It",
+
+	description: "Permanently delete your Tick It account and remove associated content."
+};
+
 export default function DeleteAccountPage() {
 	return (
 		<DeleteAccount/>
