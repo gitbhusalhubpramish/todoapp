11b3ef4b173f8fa370057b3922794d7b571571b2
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 15:32:07 +0545

    added ttl and des to search

--- todo/app/(protected)/setting/page.js
+++ todo/app/(protected)/setting/page.js
@@ -1,8 +1,10 @@
 import Setting from "./setting.js"
+
 export const metadata = {
 	title: "Account Settings | Tick It",
 	description: "Manage your Tick It account settings, profile information, privacy preferences, security options, and account customization.",
 };
+
 export default function Settingpage(){
 	return (
 		<Setting/>
--- todo/app/search/page.js
+++ todo/app/search/page.js
@@ -1,5 +1,11 @@
 import Search from "./search.js"
 
+export const metadata = {
+	title: "Search Projects, Tasks & Users | Tick It",
+
+	description: "Discover projects, creators, productivity workflows, and public task boards across Tick It."
+};
+
 export default function SearchPage() {
 	return (
 		<Search/>
