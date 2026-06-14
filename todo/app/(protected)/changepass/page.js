a9844dd63f766e0d91e986a2de99c5b5a1ea3b03
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 15:02:50 +0545

    change pass ttl and des

--- todo/app/(protected)/changepass/page.js
+++ todo/app/(protected)/changepass/page.js
@@ -1,5 +1,11 @@
 import ChangePassword from "./changepass.js"
 
+export const metadata = {
+	title: "Change Password | Tick It",
+
+	description: "Update your Tick It account password and keep your account secure."
+};
+
 export default function ChangePasswordPage() {
 	return (
 		<ChangePassword/>
