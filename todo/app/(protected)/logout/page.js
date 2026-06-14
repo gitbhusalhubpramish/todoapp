56f1e2901a1aff62cd51d528678365e6b95f920d
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-14 14:48:35 +0545

    added title and des for logout

--- todo/app/(protected)/logout/page.js
+++ todo/app/(protected)/logout/page.js
@@ -1,4 +1,11 @@
 import LogoutButton from "./logout.js"
+
+export const metadata = {
+	title: "Sign Out | Tick It",
+
+	description:"Securely sign out of your Tick It account."
+};
+
 export default function LogoutButtonpage() {
 	return (
 		<LogoutButton/>
