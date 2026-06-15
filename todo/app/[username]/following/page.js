cb0d21934f27e710cbcc232391a293b061bd2551
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-15 18:34:17 +0545

    following page ttl and des

--- todo/app/[username]/following/page.js
+++ todo/app/[username]/following/page.js
@@ -1,4 +1,15 @@
 import Following from "./following.js"
+
+export async function generateMetadata({ params }) {
+	const { username } = await params;
+
+	return {
+		title: `${username} Following | Tick It`,
+
+		description: `See creators, projects, and productivity enthusiasts followed by ${username}.`
+	};
+}
+
 export default async function followingpage({ params }){
 	//get target username
 	const {username} = await params
