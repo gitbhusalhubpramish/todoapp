389ef5962be85db7bfd562244ba42a08d61907b9
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-15 18:42:20 +0545

    project file ttl and des

--- todo/app/[username]/[project]/page.js
+++ todo/app/[username]/[project]/page.js
@@ -1,5 +1,14 @@
 import Project from "./project.js"
 
+export async function generateMetadata({ params }) {
+	const { username, project } = await params;
+
+	return {
+		title: `${project} by ${username} | Tick It`,
+		description: `View ${project}, a project by ${username} on Tick It.`,
+	};
+}
+
 export default async function project({ params }) {
 	//get target username
 	const { username, project } = await params
