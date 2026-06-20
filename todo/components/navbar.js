a608fd3dcf3f413fef4ff2e578c442ed408b18fb
gitbhusalhubpramish <161274817+gitbhusalhubpramish@users.noreply.github.com>
2026-06-20 13:48:14 +0545

    some changes with logo

--- todo/components/navbar.js
+++ todo/components/navbar.js
@@ -1,16 +1,31 @@
-import UsrNav from "./usrnav.js"
-import Search from "./search.js"
-export default function Navbar(){
-	
+import UsrNav from "./usrnav.js";
+import Search from "./search.js";
+
+export default function Navbar() {
 	return (
-		<nav className="h-30 fixed z-20 insert-0 w-full bg-[#00c950] dark:bg-[#060a15] border-b-2 dark:border-[#04060d] border-[#149d4b] flex justify-center items-center">
-			<div className="w-full flex">
-				<div className="sm:w-1/4 flex items-center">
-					<h1 className="sm:ml-10 ml-7 mr-5 text-2xl dark:text-green-500 font-bold"><a href="/"><img src= "/tickit.png" alt="TickIt"/></a></h1>
+		<nav className="fixed top-0 left-0 right-0 z-20 h-30 bg-[#00c950] dark:bg-[#060a15] border-b-2 border-[#149d4b] dark:border-[#04060d]">
+			<div className="max-w-7xl mx-auto h-full flex items-center">
+				
+				{/* Logo */}
+				<div className="flex items-center px-4 sm:px-8">
+					<a
+						href="/"
+						className="flex items-center transition-transform duration-200 hover:scale-105"
+					>
+						<img
+							src="/tickit.png"
+							alt="TickIt"
+							className="h-15 w-auto object-contain"
+						/>
+					</a>
 				</div>
-				<Search/>
-				<UsrNav/>
+
+				{/* Search */}
+				<Search />
+
+				{/* User Navigation */}
+				<UsrNav />
 			</div>
 		</nav>
-	)
+	);
 }
